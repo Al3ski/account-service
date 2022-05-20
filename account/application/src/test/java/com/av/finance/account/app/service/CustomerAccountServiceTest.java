c72939b783da3debc5666ef21093a1cb5b09adfa
Aleksey Vtyurin <About269!@>
2022-05-20 23:46:16 +0300

    Create application service test

---
+++ account/application/src/test/java/com/av/finance/account/app/service/CustomerAccountServiceTest.java
@@ -0,0 +1,81 @@
+package com.av.finance.account.app.service;
+
+import com.av.finance.account.common.BusinessException;
+import com.av.finance.account.domain.account.CustomerAccount;
+import com.av.finance.account.domain.account.repository.CustomerAccountRepository;
+import com.av.finance.account.domain.customer.Customer;
+import com.av.finance.account.domain.customer.repository.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static com.av.finance.account.domain.account.CustomerAccountType.CURRENT;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(SpringExtension.class)
+class CustomerAccountServiceTest {
+
+    @Autowired
+    private CustomerAccountService customerAccountService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+    @MockBean
+    private CustomerAccountRepository customerAccountRepository;
+
+    @Test
+    void openCurrentAccount_success() {
+        final UUID customerId = UUID.randomUUID();
+
+        final Customer customer = prepareCustomer(customerId);
+
+        Mockito.when(customerRepository.retrieve(customerId)).thenReturn(customer);
+        Mockito.doNothing().when(customerAccountRepository).save(any(CustomerAccount.class));
+
+        final UUID accountId = customerAccountService.openAccount(customerId, CURRENT, BigDecimal.ZERO);
+
+        Assertions.assertNotNull(accountId);
+
+        Mockito.verify(customerRepository, times(1)).retrieve(customerId);
+        Mockito.verify(customerAccountRepository, times(1)).save(any(CustomerAccount.class));
+    }
+
+    @Test
+    void openCurrentAccount_withNegativeBalance_fail() {
+        final UUID customerId = UUID.randomUUID();
+
+        final Customer customer = prepareCustomer(customerId);
+
+        Mockito.when(customerRepository.retrieve(customerId)).thenReturn(customer);
+
+        final BusinessException exception = Assertions.assertThrows(
+                BusinessException.class,
+                () -> customerAccountService.openAccount(customerId, CURRENT, BigDecimal.valueOf(-1L))
+        );
+
+        Assertions.assertEquals("Customer balance must be >= 0", exception.getMessage());
+    }
+
+    private Customer prepareCustomer(UUID customerId) {
+        return Customer.builder()
+                .customerId(customerId)
+                .name("username")
+                .surname("surname")
+                .build();
+    }
+
+    @TestConfiguration
+    @ComponentScan(basePackages = "com.av.finance.account.app.service")
+    static class CustomerAccountServiceConfiguration {
+    }
+}
