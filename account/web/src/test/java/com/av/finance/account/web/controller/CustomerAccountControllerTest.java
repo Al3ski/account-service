055080553a1556bf78003b9b8e4f2a2b4c90d64f
Aleksey Vtyurin <About269!@>
2022-05-27 23:32:12 +0300

    Add additional unit tests for CustomerAccountController

--- account/web/src/test/java/com/av/finance/account/web/controller/CustomerAccountControllerTest.java
+++ account/web/src/test/java/com/av/finance/account/web/controller/CustomerAccountControllerTest.java
@@ -4,6 +4,7 @@ import com.av.finance.account.app.service.CustomerAccountService;
 import com.av.finance.account.domain.account.CustomerAccountType;
 import com.av.finance.account.web.dto.AccountInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static java.util.Collections.emptyList;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -37,7 +41,7 @@ class CustomerAccountControllerTest {
     private CustomerAccountService customerAccountService;
 
     @Test
-    void openCurrentAccountRequest_returnOk() throws Exception {
+    void openCurrentAccountRequest_returnCreated() throws Exception {
         final AccountInput accountInput = new AccountInput(
                 UUID.randomUUID(),
                 CustomerAccountType.CURRENT,
@@ -48,12 +52,28 @@ class CustomerAccountControllerTest {
                 any(BigDecimal.class))).thenReturn(accountInput.getCustomerId());
 
         mockMvc.perform(
-                post("/v1/accounts")
-                        .content(objectMapper.writeValueAsString(accountInput))
-                        .contentType(MediaType.APPLICATION_JSON))
+                        post("/v1/accounts")
+                                .content(objectMapper.writeValueAsString(accountInput))
+                                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(header().string(LOCATION, containsString("/v1/accounts/" + accountInput.getCustomerId())));
+    }
+
+    @Test
+    void getAccountsDetailsRequest_returnOk() throws Exception {
+        Mockito.when(customerAccountService.getCustomerAccountsDetails(any(UUID.class))).thenReturn(emptyList());
+
+        final MvcResult result = mockMvc.perform(
+                        get("/v1/accounts/details")
+                                .param("customer_id", UUID.randomUUID().toString())
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Assertions.assertNotNull(result.getResponse().getContentAsString());
     }
 
 
