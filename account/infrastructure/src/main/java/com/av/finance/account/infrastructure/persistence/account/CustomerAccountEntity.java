19415afb1f0c94207366611bf6c5d86ce2d6eeae
Aleksey Vtyurin <About269!@>
2022-08-03 15:04:22 +0300

    App JPA Entities refactoring

--- account/infrastructure/src/main/java/com/av/finance/account/infrastructure/persistence/account/CustomerAccountEntity.java
+++ account/infrastructure/src/main/java/com/av/finance/account/infrastructure/persistence/account/CustomerAccountEntity.java
@@ -3,9 +3,12 @@ package com.av.finance.account.infrastructure.persistence.account;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
@@ -19,9 +22,13 @@ import java.util.UUID;
 public class CustomerAccountEntity {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "uuid-char")
     @Column(updatable = false, nullable = false)
     private UUID accountId;
 
+    @Type(type = "uuid-char")
     @Column(updatable = false, nullable = false)
     private UUID customerId;
 
